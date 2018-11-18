97423f7548431227995249ac2994b6a2c8ca8638
packsendme <packsendme@gmail.com>
2018-11-18 18:12:29 +0100

    NewVersion04

--- src/main/java/com/packsendme/microservice/account/service/AccountService.java
+++ src/main/java/com/packsendme/microservice/account/service/AccountService.java
@@ -43,7 +43,8 @@ public class AccountService {
 			accountSave = accountDAO.add(account); 
 			if(accountSave != null) {
 				// Call IAMService - To allows User Access 
-				ResponseEntity<?> userAccessEnable = iamClient.allowsFirstUserAccess(account.getUsername(),account.getPassword());
+				ResponseEntity<?> userAccessEnable = iamClient.allowsFirstUserAccess(account.getUsername(),
+						account.getPassword(), accountDto.getDtAction());
 				if(userAccessEnable.getStatusCode() == HttpStatus.OK) {
 					return new ResponseEntity<>(responseObj, HttpStatus.ACCEPTED);
 				}
