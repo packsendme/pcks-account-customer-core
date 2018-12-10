36910cec289a1ed16f422dc352ec8f56f667296b
packsendme <packsendme@gmail.com>
2018-12-10 12:44:01 +0100

    NewVersion-00-payment

---
+++ src/main/java/com/packsendme/microservice/account/repository/PaymentModel.java
@@ -0,0 +1,67 @@
+package com.packsendme.microservice.account.repository;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Payment")
+public class PaymentModel implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	@Id
+    private String id;
+    private String cardName;
+	private String cardNumber;
+	private Date cardExpiry;
+	private String cardCVV;
+	private String cardCountry;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCardName() {
+		return cardName;
+	}
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public Date getCardExpiry() {
+		return cardExpiry;
+	}
+	public void setCardExpiry(Date cardExpiry) {
+		this.cardExpiry = cardExpiry;
+	}
+	public String getCardCVV() {
+		return cardCVV;
+	}
+	public void setCardCVV(String cardCVV) {
+		this.cardCVV = cardCVV;
+	}
+	public String getCardCountry() {
+		return cardCountry;
+	}
+	public void setCardCountry(String cardCountry) {
+		this.cardCountry = cardCountry;
+	}
+
+
+	
+}
