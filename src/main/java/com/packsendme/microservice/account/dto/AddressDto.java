b9177991e15614ab7cb084df98bf59b6bcdeead7
ricardomarzochi <ricardomarzochi@gmail.com>
2019-06-10 17:34:39 +0200

    newVersion-001

--- src/main/java/com/packsendme/microservice/account/dto/AddressDto.java
+++ src/main/java/com/packsendme/microservice/account/dto/AddressDto.java
@@ -1,7 +1,6 @@
 package com.packsendme.microservice.account.dto;
 
 import java.io.Serializable;
-import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -19,15 +18,8 @@ public class AddressDto implements Serializable {
 	@Id
 	private String id;
 	private String address;
-	private String number;
-	private String stateorprovince;
-	private String postalcode;
-	private String city;
-	private String country;
 	private String type;
-	private String composite;
-	private String latitude;
-	private String longitude;
+	private String main;
 	
 	
 	public String getAddress() {
@@ -36,65 +28,23 @@ public class AddressDto implements Serializable {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
-	public String getStateorprovince() {
-		return stateorprovince;
-	}
-	public void setStateorprovince(String stateorprovince) {
-		this.stateorprovince = stateorprovince;
-	}
-	public String getPostalcode() {
-		return postalcode;
-	}
-	public void setPostalcode(String postalcode) {
-		this.postalcode = postalcode;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
 	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
 		this.type = type;
 	}
-	public String getComposite() {
-		return composite;
-	}
-	public void setComposite(String composite) {
-		this.composite = composite;
-	}
-	public String getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
-	public String getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(String longitude) {
-		this.longitude = longitude;
-	}
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public String getMain() {
+		return main;
+	}
+	public void setMain(String main) {
+		this.main = main;
 	}
 	
 	
